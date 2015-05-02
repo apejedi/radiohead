c7a3f43e62248bd4151072f93ef93d2e9f08f38e
unknown <umrai002@d.umn.edu>
2015-05-02 16:11:08 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to radiohead
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/radiohead/core_test.clj
@@ -0,0 +1,7 @@
+(ns radiohead.core-test
+  (:require [clojure.test :refer :all]
+            [radiohead.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
